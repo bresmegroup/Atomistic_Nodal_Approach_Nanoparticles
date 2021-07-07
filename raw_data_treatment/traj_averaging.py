e0df2f3ce97699d6fd891478c4b4da62142088be
Mingxuan Jiang <40763563+PanoptoSalad@users.noreply.github.com>
2021-07-07 20:59:08 +0800

    Update traj_averaging.py
    Code now gets filename and directory from a single string

--- raw_data_treatment/traj_averaging.py
+++ raw_data_treatment/traj_averaging.py
@@ -14,11 +14,21 @@ args = sys.argv[1:]
 no_atom = args[0] # USER input required here, to state the number of atoms involved
 file_name = args[1] # USER input required here, to state what the general name of the files are for averaging
 
-df = dd.read_csv('{}*'.format(file_name), sep = ' ', skiprows = 17 + no_atom) # 17 trajectory labels that dont contribute to the data, and lammps output for t = 0 set to 0 removed
+directory = os.path.dirname(file_name) # get directory from file name
+fn = os.path.basename(file_name)
+
+file_no = len(fnmatch.filter(os.listdir(directory)
+                         , '{}*'.format(fn))) #added a bracket
+
+df = dd.read_csv(r'{}*'.format(file_name), sep = ' ', skiprows = 17 + no_atom, sample=1000000) # 17 trajectory labels that dont contribute to the data, and lammps output for t = 0 set to 0 removed
+print('dataframe loaded')
 columns_l = df.columns
 #df = df.dropna(axis=1)
 df.columns = list(columns_l[2:-1]) + ['t1','t2','t3']
 df = df[columns_l[2:-1]]
+print('Columns added')
 
 df_means = df.groupby(df.id).mean().compute()
-df_means.to_csv('traj_means_{}.csv'.format(start, end)) # output the .csv file of averaged trajectory file
+df_means.to_csv('traj_means_{}_{}.csv'.format(0, file_no)) # output the .csv file of averaged trajectory file
+
+print('Filed Saved')
